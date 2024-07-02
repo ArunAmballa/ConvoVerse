33c22cbeb0b0a1c7ca9d97d8aac6f7ac119bba0a
Arun Amballa <arunamballa@Aruns-MacBook-Air.local>
2024-07-01 20:34:18 -0700

    Established Web Socket Connection and Added Redis

--- auth-server/db/database.js
+++ auth-server/db/database.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const connectDB=async(req,res)=>{
     try{
 
+        
+
         await mongoose.connect(process.env.MONGODB_URL);
         console.log("Connection to DB is Successfull");
 
